8bf5bc8e65202bf8f4a689dd5cd0b0ddf3809cbf
Vitaliy Pyza <vitaliy.pyza@globallogic.com>
2018-02-07 14:03:17 +0200

    fill back

---
+++ Console/dll/XML.h
@@ -0,0 +1,19 @@
+#ifndef XML_h_
+#define XML_h_
+#include"tinyxml.h"
+#include"Table.h"
+
+MYLIBRARY_EXPORT void CreateXML();
+MYLIBRARY_EXPORT unsigned int Count();
+MYLIBRARY_EXPORT bool NeedsToBeUpdated(Table& tb);
+MYLIBRARY_EXPORT void AddStructure(Table& source);
+MYLIBRARY_EXPORT void AddData(Table& source);
+MYLIBRARY_EXPORT Table* BuildTable(Table& tb, unsigned int id);
+MYLIBRARY_EXPORT void DeleteTable(unsigned int id);
+MYLIBRARY_EXPORT void Clear();
+MYLIBRARY_EXPORT std::string GetName(unsigned int ID);
+MYLIBRARY_EXPORT int* GetID();
+MYLIBRARY_EXPORT int GetLastID();
+MYLIBRARY_EXPORT void GColector();
+	
+#endif
